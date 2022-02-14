95473d6f8cf22b2b0c91738007cec34ad40ce3b0
Naïm db <ndb@Mac-mini-de-Naim.local>
2022-02-14 17:49:46 +0100

    init prisma

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const { PrismaClient } = require('@prisma/client')
 const app = fastify({ logger: true })
 const prisma = new PrismaClient()
 
+async function main() {
+    await prisma.$connect()
+}
+
 app.register(require('./routes/first'))
 app.register(require('./db-connector'))
 
@@ -15,5 +19,13 @@ const start = async () => {
         process.exit(1)
     }
 }
+
+main()
+    .catch((e) => {
+        throw e
+    })
+    .finally(async () => {
+        await prisma.$disconnect()
+    })
 
 start()
